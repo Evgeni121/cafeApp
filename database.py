62e365181f0fbe66afed167330058b4ef2186125
blahadareu.y <darevblago121@mail.ru>
2026-01-26 17:56:49 +0300

    12

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine, Table, MetaData, Column, Integer, String, select
+from sqlalchemy import create_engine, Table, MetaData, Column, Integer, String, select, Boolean
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
@@ -10,7 +10,6 @@ if not address:
     raise "Get DATABASE_ADDRESS error!"
 
 engine = create_engine(f"{address}/postgres", echo=False)
-
 Session = sessionmaker(engine, expire_on_commit=False)
 
 metadata_obj = MetaData()
@@ -19,6 +18,15 @@ cafe_user_table = Table(
     "cafe_user",
     metadata_obj,
     Column("id", Integer, primary_key=True),
+    Column("cafe_id", Integer),
+    Column("is_barista", Boolean)
 )
 
-stmt = select(cafe_user_table).where(cafe_user_table.c.is_barista is True)
+with Session() as session:
+    stmt = (select(cafe_user_table)
+            .where(cafe_user_table.c.is_barista == True)
+            .where(cafe_user_table.c.cafe_id == 1)
+            )
+
+    for row in session.execute(stmt):
+        print(row)
